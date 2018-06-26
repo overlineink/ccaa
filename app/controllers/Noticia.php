7f7f4c3ce670c36e959bea6a9cf80a3d44e1abb0
Jorge Dacosta <JorgeMSDC@mail.com>
2018-06-26 13:09:59 +0100

    add noticia controller

---
+++ app/controllers/Noticia.php
@@ -0,0 +1,14 @@
+<?php
+    class Noticia extends Controller {
+
+        public function __construct($controller, $action) {
+            parent:__construct($controller, $action);
+        }
+
+        # to display single notice
+        public function indexAction() {
+        }
+
+        # to display a list of notices
+        public function recentesAction() {}
+    }
