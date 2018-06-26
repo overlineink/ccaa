f7581a5fcdadfac3deaeeb06b07decacfba7b6e5
Jorge Dacosta <JorgeMSDC@mail.com>
2018-06-26 14:26:03 +0100

    improve contacto page design

--- app/views/contacto/index.php
+++ app/views/contacto/index.php
@@ -10,6 +10,14 @@
 <?php $this->end(); ?>
 
 <?php $this->start('body'); ?>
+<div class="wt-bnr-inr overlay-wraper" style="background-image:url(https://images.unsplash.com/photo-1511805296765-e8de8e3750f4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=aa35de728b473ed2ed31b592b8b37483&auto=format&fit=crop&w=1479&q=80);">
+    <div class="overlay-main bg-black opacity-07"></div>
+    <div class="container">
+        <div class="wt-bnr-inr-entry">
+            <h1 class="text-white">Envie-nos um texto ou solicite uma chamada</h1>
+        </div>
+    </div>
+</div>
 <div class="bg-gray-light p-tb20">
     <div class="container">
         <ul class="wt-breadcrumb breadcrumb-style-2">
