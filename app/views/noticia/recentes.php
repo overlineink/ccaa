2d5900f1fc8398b2dfce0cd139155f7c8596882c
Jorge Dacosta <JorgeMSDC@mail.com>
2018-06-26 13:20:51 +0100

    refractor [rename view name]

---
+++ app/views/noticia/recentes.php
@@ -0,0 +1,13 @@
+
+<?php $this->start('body'); ?>
+
+<?php $this->end(); ?>
+
+<?php $this->start('head'); ?>
+<!-- Custom styles for this template -->
+<?php $this->end(); ?>
+
+<?php $this->start('scripts'); ?>
+<!-- Scripts buffer works! -->
+<?php $this->end(); ?>
+
