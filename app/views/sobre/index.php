3e333172693a47b6f7d5137d67bd899924d596ec
Jorge Dacosta <JorgeMSDC@mail.com>
2018-06-29 13:06:25 +0100

    Adicionando conteudo ao Quem Somos +View

--- app/views/sobre/index.php
+++ app/views/sobre/index.php
@@ -1,2 +1,19 @@
 <?php $this->start('body'); ?>
+<div class="wt-bnr-inr overlay-wraper" style="background-image:url(https://images.unsplash.com/photo-1511805296765-e8de8e3750f4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=aa35de728b473ed2ed31b592b8b37483&auto=format&fit=crop&w=1479&q=80);">
+    <div class="overlay-main bg-black opacity-07"></div>
+    <div class="container">
+        <div class="wt-bnr-inr-entry">
+            <h1 class="text-white">Envie-nos um texto ou solicite uma chamada</h1>
+            <p class="text-white" style="margin-top: -23px;">O nosso cliente é uma empresa de referência no setor dos seguros que se encontra de momento a recrutar...</p>
+        </div>
+    </div>
+</div>
+<div class="bg-gray-light p-tb20">
+    <div class="container">
+        <ul class="wt-breadcrumb breadcrumb-style-2">
+            <li><a href="<?=PROOT?>"><i class="fa fa-home"></i> Página Inicial</a></li>
+            <li>Quem Somos</li>
+        </ul>
+    </div>
+</div>
 <?php $this->end();
