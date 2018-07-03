af887371682ee51cc683e8993e29f31687fb76b9
Jorge Dacosta <JorgeMSDC@mail.com>
2018-07-03 13:20:23 +0100

    refractor design

--- app/views/galeria/index.php
+++ app/views/galeria/index.php
@@ -10,4 +10,206 @@
         </ul>
     </div>
 </div>
+
+<div class="section-full p-t80 p-b50">
+    <div class="container">
+    
+        <!-- PAGINATION TOP START -->
+        <div class="filter-wrap p-tb15">
+            <ul class="masonry-filter link-style  text-uppercase">
+                <li class="active"><a data-filter="*" href="#">All</a></li>
+                <li class=""><a data-filter=".cat-1" href="#">House</a></li>
+                <li class=""><a data-filter=".cat-2" href="#">Building</a></li>
+                <li class=""><a data-filter=".cat-3" href="#">Office</a></li>
+                <li class=""><a data-filter=".cat-4" href="#">Garden</a></li>
+                <li class=""><a data-filter=".cat-5" href="#">Interior</a></li>
+            </ul>
+        </div>
+        <!-- PAGINATION END -->
+        
+        <!-- GALLERY CONTENT START -->
+        <div class="row">
+            <div class="portfolio-wrap mfp-gallery no-col-gap" style="position: relative; height: 766.077px;">
+            
+                <!-- COLUMNS 1 -->
+                <div class="masonry-item cat-1 col-lg-4 col-md-4 col-sm-6 col-xs-6" style="position: absolute; left: 0px; top: 0px;">
+                    <div class="wt-gallery-bx p-a15">
+                        <div class="wt-thum-bx wt-img-overlay5 wt-img-effect blurr">
+                            <a href="javascript:void(0);">
+                                <img src="http://thewebmax.com/build/images/latest-projects/pic1.jpg" alt="">
+                            </a>
+                            <div class="overlay-bx">
+                                <div class="overlay-icon">
+                                    <a href="javascript:void(0);">
+                                        <i class="fa fa-external-link wt-icon-box-xs"></i>
+                                    </a>
+                                    <a href="http://thewebmax.com/build/images/gallery/pic1.jpg" class="mfp-link">
+                                        <i class="fa fa-arrows-alt wt-icon-box-xs"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- COLUMNS 2 -->
+                <div class="masonry-item cat-2 col-lg-4 col-md-4 col-sm-6 col-xs-6" style="position: absolute; left: 390px; top: 0px;">
+                    <div class="wt-gallery-bx p-a15">
+                        <div class="wt-thum-bx wt-img-overlay5 wt-img-effect blurr">
+                            <a href="javascript:void(0);">
+                                <img src="http://thewebmax.com/build/images/latest-projects/pic2.jpg" alt="">
+                            </a>
+                            <div class="overlay-bx">
+                                <div class="overlay-icon">
+                                    <a href="javascript:void(0);">
+                                        <i class="fa fa-external-link wt-icon-box-xs"></i>
+                                    </a>
+                                    <a href="http://thewebmax.com/build/images/gallery/pic2.jpg" class="mfp-link">
+                                        <i class="fa fa-arrows-alt wt-icon-box-xs"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- COLUMNS 3 -->
+                <div class="masonry-item cat-3 col-lg-4 col-md-4 col-sm-6 col-xs-6" style="position: absolute; left: 780px; top: 0px;">
+                    <div class="wt-gallery-bx p-a15">
+                        <div class="wt-thum-bx wt-img-overlay5 wt-img-effect blurr">
+                            <a href="javascript:void(0);">
+                                <img src="http://thewebmax.com/build/images/latest-projects/pic3.jpg" alt="">
+                            </a>
+                            <div class="overlay-bx">
+                                <div class="overlay-icon">
+                                    <a href="javascript:void(0);">
+                                        <i class="fa fa-external-link wt-icon-box-xs"></i>
+                                    </a>
+                                    <a href="http://thewebmax.com/build/images/gallery/pic3.jpg" class="mfp-link">
+                                        <i class="fa fa-arrows-alt wt-icon-box-xs"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- COLUMNS 4 -->
+                <div class="masonry-item cat-4 col-lg-4 col-md-4 col-sm-6 col-xs-6" style="position: absolute; left: 0px; top: 255px;">
+                    <div class="wt-gallery-bx p-a15">
+                        <div class="wt-thum-bx wt-img-overlay5 wt-img-effect blurr">
+                            <a href="javascript:void(0);">
+                                <img src="http://thewebmax.com/build/images/latest-projects/pic4.jpg" alt="">
+                            </a>
+                            <div class="overlay-bx">
+                                <div class="overlay-icon">
+                                    <a href="javascript:void(0);">
+                                        <i class="fa fa-external-link wt-icon-box-xs"></i>
+                                    </a>
+                                    <a href="http://thewebmax.com/build/images/gallery/pic4.jpg" class="mfp-link m-b15">
+                                        <i class="fa fa-arrows-alt wt-icon-box-xs"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- COLUMNS 5 -->
+                <div class="masonry-item cat-5 col-lg-4 col-md-4 col-sm-6 col-xs-6" style="position: absolute; left: 390px; top: 255px;">
+                    <div class="wt-gallery-bx p-a15">
+                        <div class="wt-thum-bx wt-img-overlay5 wt-img-effect blurr">
+                            <a href="javascript:void(0);">
+                                <img src="http://thewebmax.com/build/images/latest-projects/pic5.jpg" alt="">
+                            </a>
+                            <div class="overlay-bx">
+                                <div class="overlay-icon">
+                                    <a href="javascript:void(0);">
+                                        <!-- <i class="fa fa-external-link wt-icon-box-xs"></i> -->
+                                    </a>
+                                    <a href="http://thewebmax.com/build/images/gallery/pic5.jpg" class="mfp-link">
+                                        <i class="fa fa-arrows-alt wt-icon-box-xs"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- COLUMNS 6 -->
+                <div class="masonry-item cat-4 col-lg-4 col-md-4 col-sm-6 col-xs-6" style="position: absolute; left: 780px; top: 255px;">
+                    <div class="wt-gallery-bx p-a15">
+                        <div class="wt-thum-bx wt-img-overlay5 wt-img-effect blurr">
+                            <a href="javascript:void(0);">
+                                <img src="http://thewebmax.com/build/images/latest-projects/pic6.jpg" alt="">
+                            </a>
+                            <div class="overlay-bx">
+                                <div class="overlay-icon">
+                                    <a href="javascript:void(0);">
+                                        <i class="fa fa-external-link wt-icon-box-xs"></i>
+                                    </a>
+                                    <a href="http://thewebmax.com/build/images/gallery/pic6.jpg" class="mfp-link">
+                                        <i class="fa fa-arrows-alt wt-icon-box-xs"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- COLUMNS 7 -->
+                <div class="masonry-item cat-3 col-lg-4 col-md-4 col-sm-6 col-xs-6" style="position: absolute; left: 0px; top: 510px;">
+                    <div class="wt-gallery-bx p-a15">
+                        <div class="wt-thum-bx wt-img-overlay5 wt-img-effect blurr">
+                            <a href="javascript:void(0);">
+                                <img src="http://thewebmax.com/build/images/latest-projects/pic7.jpg" alt="">
+                            </a>
+                            <div class="overlay-bx">
+                                <div class="overlay-icon">
+                                    <a href="javascript:void(0);">
+                                        <i class="fa fa-external-link wt-icon-box-xs"></i>
+                                    </a>
+                                    <a href="http://thewebmax.com/build/images/gallery/pic7.jpg" class="mfp-link">
+                                        <i class="fa fa-arrows-alt wt-icon-box-xs"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- COLUMNS 8 -->
+                <div class="masonry-item cat-2 col-lg-4 col-md-4 col-sm-6 col-xs-6" style="position: absolute; left: 390px; top: 510px;">
+                    <div class="wt-gallery-bx p-a15">
+                        <div class="wt-thum-bx wt-img-overlay5 wt-img-effect blurr">
+                            <a href="javascript:void(0);">
+                                <img src="http://thewebmax.com/build/images/latest-projects/pic8.jpg" alt="">
+                            </a>
+                            <div class="overlay-bx">
+                                <div class="overlay-icon">
+                                    <a href="javascript:void(0);">
+                                        <i class="fa fa-external-link wt-icon-box-xs"></i>
+                                    </a>
+                                    <a href="http://thewebmax.com/build/images/gallery/pic8.jpg" class="mfp-link">
+                                        <i class="fa fa-arrows-alt wt-icon-box-xs"></i>
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            
+            </div>
+        </div>
+        <!-- GALLERY CONTENT END -->
+        
+        <!-- PAGINATION START -->
+        <div class="pagination-bx  clearfix ">
+            <ul class="custom-pagination pagination">
+                <li><a href="#">«</a></li>
+                <li class="active"><a href="#">1</a></li>
+                <li><a href="#">2</a></li>
+                <li><a href="#">3</a></li>
+                <li><a href="#">4</a></li>
+                <li><a href="#">5</a></li>
+                <li><a href="#">»</a></li>
+            </ul>
+        </div>
+        <!-- PAGINATION END -->                    
+        
+    </div>
+</div>
 <?php $this->end(); ?>
