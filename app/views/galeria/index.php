a70e218a49b6f659178d7f9bd0ce3daa42f4d764
Jorge Dacosta <JorgeMSDC@mail.com>
2018-07-03 12:58:03 +0100

    refractor `Galeria` design

--- app/views/galeria/index.php
+++ app/views/galeria/index.php
@@ -1,9 +1,13 @@
 <?php $this->start('body'); ?>
-<div class="container"> 
-    <div class="row">
-        <div class="col-md-9 col-sm-9">
-            <h2 class="weight-600">Galeria works!</h2>
-        </div>
+<div class="wt-bnr-inr overlay-wraper bg-parallax" style="background-image:url(<?=PROOT?>images/tpl.jpg);">
+    <div class="overlay-main bg-black"></div>
+</div>
+<div class="bg-gray-light p-tb20">
+    <div class="container">
+        <ul class="wt-breadcrumb breadcrumb-style-2">
+            <li><a href="<?=PROOT?>"><i class="fa fa-home"></i> Portfolio</a></li>
+            <li>Galeria</li>
+        </ul>
     </div>
 </div>
 <?php $this->end(); ?>
